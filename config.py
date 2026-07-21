a5943879eaa388851724b7e0d9c8d608ba4b3659
s68181791-bit <s68181791@gmail.com>
2026-07-21 17:22:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 import os
 
 BOT_TOKEN = os.getenv("8834827673:AAGwzMP__APAGsQ5IxKxEhEkfvxECLbWt_M")
+
+ADMIN_ID = 5222385918  # Замени на свой Telegram ID
+
+VPN_LINK = "https://raw.githubusercontent.com/s68181791-bit/-santizz/main/LTE.txt"
+
+PRICE = 150
+
+SUPPORT = "@santizz00"
